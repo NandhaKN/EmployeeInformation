de4bc1569fa0ba642f01d49a05fb977ca4052cdf
snandha99 <snandha99@gmail.com>
2022-01-09 22:09:31 +0530

    commit

--- Documents/eclipse projects/EmployeeInformation/src/org/client/Client.java
+++ Documents/eclipse projects/EmployeeInformation/src/org/client/Client.java
@@ -4,6 +4,10 @@ public class Client {
 	private void clientName() {
 		System.out.println("And the client Name is UBER");
 	}
+	public class Client {
+		private void clientId() {
+			System.out.println("And the client Id is 12345");
+		}
 public static void main(String[] args) {
 	Client b = new Client();
 	b.clientName();
